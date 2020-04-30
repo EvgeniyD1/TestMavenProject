daeec52423db05abd50c2cfc8eefb10e46feddc1
EvgeniyD1 <edolgiyj7@gmail.com>
2020-04-30 16:46:18 +0300

    Initial commit

---
+++ src/main/java/com/htp/Main.java
@@ -0,0 +1,11 @@
+package com.htp;
+
+import org.apache.commons.lang3.RandomUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAAAAAAAA");
+        System.out.println(RandomUtils.nextBoolean());
+    }
+}
+
