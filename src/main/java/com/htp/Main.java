58c46e7539b9409adb13d1ff996b87780d428e51
EvgeniyD1 <edolgiyj7@gmail.com>
2020-05-04 14:27:22 +0300

    .gitignore

--- src/main/java/com/htp/Main.java
+++ src/main/java/com/htp/Main.java
@@ -4,9 +4,10 @@ import org.apache.commons.lang3.RandomUtils;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("AAAAAAAAAAAAAAAAAA");
+        System.out.println("AAA");
         System.out.println(RandomUtils.nextBoolean());
         System.out.println(":new line");
+        System.out.println("BBB");
     }
 }
 
